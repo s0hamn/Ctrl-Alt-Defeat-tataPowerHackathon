d1b231b33a81f9c76d6b87d4912c52be98aca12f
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2023-10-28 07:54:08 +0530

    id

---
+++ global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var points = 0
+@onready var is_correct_wind = false
+@onready var is_correct_solar = false
+@onready var flagWind = 0
+@onready var flagSolar = 0
+
+func _ready():
+	pass
